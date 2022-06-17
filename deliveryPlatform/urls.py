52ca5f1bb0e86e469ab3bcf861a218b16d3e8464
Jorge Cobo <cobo-angel@hotmail.com>
2022-06-17 08:26:45 -0400

    Defined the API service for the Wishlist.

--- deliveryPlatform/urls.py
+++ deliveryPlatform/urls.py
@@ -20,11 +20,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from stores import views as stores_views
 from home import views as home_views
+from wishlists import views as wishlists_views
 
 router = DefaultRouter()
 
 router.register(r'home', home_views.HomePage, basename = 'home')
 router.register(r'stores', stores_views.StoreView, basename = 'stores')
+router.register(r'wishlists', wishlists_views.WishlistView, basename='wishlists')
 
 urlpatterns = [
     path('', include(router.urls)),
