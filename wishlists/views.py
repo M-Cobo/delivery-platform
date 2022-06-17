dc9a28770bfc4a3de0a228f21713efad3cd0b0cc
Jorge Cobo <cobo-angel@hotmail.com>
2022-06-17 08:20:31 -0400

    Defined view class

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Wishlist
+from .serializers import WishlistSerializer
 
-# Create your views here.
+class WishlistView(viewsets.ModelViewSet):
+    queryset = Wishlist.objects.all()
+    serializer_class = WishlistSerializer
